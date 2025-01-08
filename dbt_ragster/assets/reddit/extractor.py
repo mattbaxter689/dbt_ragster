369fffa69b66903dfc1e59f1ddd5743527c51714
Matthew Baxter <matthewbaxter0412@hotmail.com>
2025-01-08 18:30:39 -0500

    Adding extraction task for reddit. Will improve

--- dbt_ragster/assets/reddit/extractor.py
+++ dbt_ragster/assets/reddit/extractor.py
@@ -1,4 +1,9 @@
-from dagster import AssetExecutionContext, asset
+from dagster import (
+    AssetExecutionContext,
+    asset,
+    MetadataValue,
+    MaterializeResult,
+)
 from dbt_ragster.assets.reddit.reddit_extraction import RedditExtractor
 
 extractor = RedditExtractor(init_site="dbt_ragster")
@@ -6,5 +11,16 @@ extractor = RedditExtractor(init_site="dbt_ragster")
 
 @asset(description="Extract data from certain subreddits")
 def reddit_extraction(context: AssetExecutionContext) -> None:
-    context.log.info("Hello Dagster!!!")
-    context.log.info(f"Scopes: {extractor.get_reddit()}")
+    context.log.info(f"Scopes: {extractor.get_reddit_scopes()}")
+    post, comment = extractor.extract_subreddit_posts(
+        sub_reddits="dataengineering"
+    )
+    context.log.info(f"Posts: {post.dtypes}")
+    context.log.info(f"Comments: {comment.dtypes}")
+
+    return MaterializeResult(
+        metadata={
+            "posts": MetadataValue.md(post.head().to_markdown()),
+            "comment": MetadataValue.md(comment.head().to_markdown()),
+        }
+    )
--- dbt_ragster/assets/reddit/reddit_extraction.py
+++ dbt_ragster/assets/reddit/reddit_extraction.py
@@ -1,11 +1,78 @@
 import praw
+import pandas as pd
 
-class RedditExtractor():
+
+class RedditExtractor:
     def __init__(self, init_site: str) -> None:
         self._reddit = praw.Reddit(
-            init_site,
-            user_agent="Comment extraction by u/bananasman178"
-        )      
+            init_site, user_agent="Comment extraction by u/bananasman178"
+        )
 
-    def get_reddit(self) -> set[str]:
+    def get_reddit_scopes(self) -> set[str]:
+        """Display scopes for current refresh token
+
+        Returns:
+            set[str]: a set of the current scopes
+        """
         return self._reddit.auth.scopes()
+
+    def extract_subreddit_posts(
+        self, sub_reddits: str | list[str]
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Extract data from specified subreddits
+
+        Args:
+            sub_reddits (str | list[str]): The subreddit(s) to get data
+
+        Returns:
+            tuple[pd.DataFrame, pd.DataFrame]: the posts and comment data
+        """
+
+        # convert single subreddit instance so we can iterate regardless
+        if isinstance(sub_reddits, str):
+            sub_reddits = [sub_reddits]
+
+        for reddit in sub_reddits:
+            sub_data = self._reddit.subreddit(reddit)
+            posts = []
+            comments = []
+            for post in sub_data.hot(limit=2):
+                post.comments.replace_more(limit=100)
+                for comment in post.comments:
+                    comment_data = {
+                        "body": comment.body,
+                        "author": str(comment.author),
+                        "score": comment.score,
+                        "distinguished": comment.distinguished,
+                        "edited": comment.edited,
+                        "created_utc": comment.created_utc,
+                        "is_top_level": comment.is_root,
+                        "depth": comment.depth,
+                        "gilded": comment.gilded,
+                        "post_id": comment.link_id,
+                        "comment_id": comment.id,
+                    }
+                    comments.append(comment_data)
+
+                post_data = {
+                    "title": post.title,
+                    "selftext": post.selftext,
+                    "score": post.score,
+                    "url": post.url,
+                    "id": post.id,
+                    "distinguished": post.distinguished,
+                    "locked": post.locked,
+                    "is_original_content": post.is_original_content,
+                    "spoiler": post.spoiler,
+                    "author": str(post.author),
+                    "created_utc": post.created_utc,
+                    "num_comments": post.num_comments,
+                    "upvote_ratio": post.upvote_ratio,
+                    "subreddit": str(post.subreddit),
+                }
+                posts.append(post_data)
+
+        return pd.DataFrame(posts), pd.DataFrame(comments)
+
+    def extract_post_comments(self, posts) -> pd.DataFrame:
+        pass
