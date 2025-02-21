9ffbe6bb2a5be6fa911ae1467e2a04758b32da26
Matthew Baxter <matthewbaxter0412@hotmail.com>
2025-02-21 15:13:02 -0500

    Including dbt assets in dagster definitions

--- dbt_ragster/definitions.py
+++ dbt_ragster/definitions.py
@@ -1,11 +1,15 @@
 from dagster import Definitions, load_assets_from_package_module
-
+from dagster_dbt import DbtCliResource
 from dbt_ragster.assets import reddit
+from dbt_ragster.assets import dbt
+from dbt_ragster.dbt_ragster.assets.dbt.dbt_asset import dbt_project_project
 
 reddit_assets = load_assets_from_package_module(reddit, group_name="reddit")
+dbt_assets = load_assets_from_package_module(dbt, group_name="dbt_assets")
 
-all_assets = [*reddit_assets]
+all_assets = [*reddit_assets, *dbt_assets]
 
 defs = Definitions(
     assets=all_assets,
+    resources={"dbt": DbtCliResource(project_dir=dbt_project_project)},
 )
